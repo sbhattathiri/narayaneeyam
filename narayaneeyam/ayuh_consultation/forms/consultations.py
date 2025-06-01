27d48da7be813f058cf0593d751b91ba6dd20dfa
Suraj Vasudevan Bhattathiri <svb@SVB.local>
2025-06-01 16:16:31 +0530

    requirements from vinay

--- narayaneeyam/ayuh_consultation/forms/consultations.py
+++ narayaneeyam/ayuh_consultation/forms/consultations.py
@@ -14,13 +14,51 @@ class ConsultationForm(forms.ModelForm):
             "patient",
             "doctor",
             "patient_concerns",
+            "patient_concerns_onset_from",
+            "patient_concerns_notes",
+            "ongoing_medications_notes",
             "diagnosis",
             "doctor_comments",
+            "doctor_notes",
             "next_consultation_date",
         ]
         widgets = {
-            "consultation_date": forms.TextInput(attrs={"readonly": "readonly"}),
-            "next_consultation_date": forms.widgets.DateInput(attrs={"type": "date"}),
+            "patient_concerns": forms.TextInput(
+                attrs={
+                    "placeholder": "Capture the current concerns of the patient",
+                    "class": "form-control",
+                }
+            ),
+            "patient_concerns_onset_from": forms.TextInput(
+                attrs={
+                    "placeholder": "Since how long patient has been experiencing issue?",
+                    "class": "form-control",
+                }
+            ),
+            "patient_concerns_notes": forms.TextInput(
+                attrs={
+                    "placeholder": "Other notes from patient",
+                    "class": "form-control",
+                }
+            ),
+            "doctor_notes": forms.TextInput(
+                attrs={
+                    "placeholder": "Other notes from doctor",
+                    "class": "form-control",
+                }
+            ),
+            "consultation_date": forms.TextInput(
+                attrs={
+                    "readonly": "readonly",
+                    "class": "form-control",
+                }
+            ),
+            "next_consultation_date": forms.widgets.DateInput(
+                attrs={
+                    "type": "date",
+                    "class": "form-control",
+                }
+            ),
         }
 
     def __init__(self, *args, **kwargs):
--- narayaneeyam/ayuh_consultation/models/consultations.py
+++ narayaneeyam/ayuh_consultation/models/consultations.py
@@ -10,7 +10,11 @@ from ayuh_consultation.models.appointments import (
 class Consultation(Appointment):
     consultation_date = models.DateTimeField(auto_now_add=True)
     patient_concerns = models.TextField(null=True, blank=True)
-    patient_concerns_onset_from = models.TextField(null=True, blank=True)
+    patient_concerns_onset_from = models.CharField(
+        max_length=255,
+        null=True,
+        blank=True,
+    )
     patient_concerns_notes = models.TextField(null=True, blank=True)
     ongoing_medications_notes = models.TextField(null=True, blank=True)
     diagnosis = models.TextField(null=True, blank=True)
