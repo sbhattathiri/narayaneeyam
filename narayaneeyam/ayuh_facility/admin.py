d8ae761674de20cf06d5b15130a75570047e7b80
Suraj Vasudevan Bhattathiri <svb@SVB.local>
2025-06-07 15:21:27 +0530

    black and isort

--- narayaneeyam/ayuh_facility/admin.py
+++ narayaneeyam/ayuh_facility/admin.py
@@ -1,9 +1,8 @@
-from ayuh_facility import (
-    models,
-)
 from django.contrib import (
     admin,
 )
 
-models_to_be_registered = [models.Room]
+from ayuh_facility.models.rooms import Room
+
+models_to_be_registered = [Room]
 admin.site.register(models_to_be_registered)
